939d419520b65ce4938cdfce5b7e36e5c3601f71
shawonetc322 <157136557+shawonetc322@users.noreply.github.com>
2024-01-19 20:59:35 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import {  Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View ClasName="fbg-orange-600 flex-1 justify-center items-center">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
